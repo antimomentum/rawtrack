24f55a0d144f1ee38b6b866ea2099078ffd5f2a0
antimomentum <71552326+antimomentum@users.noreply.github.com>
2023-01-16 09:46:39 -0500

    Update ESTABLISHED,RELATED.sh

--- ESTABLISHED,RELATED.sh
+++ ESTABLISHED,RELATED.sh
@@ -2,4 +2,4 @@ interface=$1
 ipset create RAWTRACK hash:ip timeout 60
 iptables -t raw -A PREROUTING -i $interface -m set --match-set RAWTRACK src -j ACCEPT
 iptables -t raw -A PREROUTING -i $interface -j DROP
-iptables -t raw -A OUTPUT -o $interface -j SET --exist --add-set RAWTRACK dst
+iptables -t mangle -A POSTROUTING -o $interface -j SET --exist --add-set RAWTRACK dst
