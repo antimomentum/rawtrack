00215214b7b069057b238a7723c1b6706fec4b67
antimomentum <71552326+antimomentum@users.noreply.github.com>
2023-01-15 21:14:12 -0500

    Update ESTABLISHED,RELATED.sh

--- ESTABLISHED,RELATED.sh
+++ ESTABLISHED,RELATED.sh
@@ -1,4 +1,4 @@
-ipset create RAWTRACK hash:ip
+ipset create RAWTRACK hash:ip  timeout 60
 iptables -t raw -A PREROUTING -i eth0 -m set --match-set RAWTRACK src -j ACCEPT
 iptables -t raw -A PREROUTING -i eth0 -j DROP
 iptables -t raw -A OUTPUT -o eth0 -j SET --exist --add-set RAWTRACK dst
