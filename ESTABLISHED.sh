07ded44545463fce3c881801bf73ccd46aac80ee
antimomentum <71552326+antimomentum@users.noreply.github.com>
2023-01-17 22:27:20 -0500

    more secure

--- ESTABLISHED.sh
+++ ESTABLISHED.sh
@@ -1,12 +1,10 @@
 interface=$1
 ipset create TEST1 hash:ip,port timeout 60
 ipset create RAWTRACK hash:ip,port timeout 60
-iptables -t mangle -N catch
 iptables -t raw -A PREROUTING -i $interface -m set --match-set TEST1 src,dst -j ACCEPT
 iptables -t raw -A PREROUTING -i $interface -j DROP
 iptables -t mangle -A PREROUTING -i $interface -m set --match-set RAWTRACK src,src -j ACCEPT
-iptables -t mangle -A PREROUTING -i $interface -m set --match-set TEST1 src,dst -j SET --exist --add-set RAWTRACK src,src
-iptables -t mangle -A PREROUTING -i $interface -j catch
-iptables -t mangle -A catch -m set --match-set RAWTRACK src,src -j ACCEPT
-iptables -t mangle -A catch -j DROP
+iptables -t mangle -A PREROUTING -i $interface -m set --match-set RAWTRACK src -j SET --del-set TEST1 src,dst
+iptables -t mangle -A PREROUTING -i $interface -m set --match-set RAWTRACK src -j DROP
+iptables -t mangle -A PREROUTING -i $interface -j SET --exist --add-set RAWTRACK src,src
 iptables -t mangle -A POSTROUTING -o $interface -j SET --exist --add-set TEST1 dst,src
